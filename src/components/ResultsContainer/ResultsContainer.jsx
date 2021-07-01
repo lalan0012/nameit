6ff1e6800b254ffea2ae7c2ac34ce3e137b65e89
Lalan Kumar <lalank0012@gmail.com>
2021-07-01 19:25:36 +0530

    project nameit

---
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import NameCard from '../NameCard/NameCard';
+import './ResultsContainer.css';
+const ResultsContainer = ({ suggestedNames }) => {
+    const suggestNameJsx = suggestedNames.map(suggestedName => {
+        return <NameCard key={suggestedName} suggestedName={suggestedName} />;
+    });
+    return (
+        <div className="results-container">
+            {suggestNameJsx}
+        </div>
+    );
+};
+export default ResultsContainer;
